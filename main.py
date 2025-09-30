90ec4aae3002ad10a9ec5f9acc1364b9e530b5bd
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-09-30 10:42:16 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import tkinter as tk
 import customtkinter as ctk
+from time import sleep
+
+# let the system pause for 3 seconds
+# to countdown 3 seconds before starting typing
+def counting_a_minute():
+    sleep(3)
+    for seconds in range(60, 0, -1):
+        remainint_secs = seconds
+        print(remainint_secs)
+    print('End')
+
+
+
+
